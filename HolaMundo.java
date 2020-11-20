5976c0715f765aff130c4e0b4a07569eb4c43fe4
Danthexz <74031922+Danthexz@users.noreply.github.com>
2020-11-19 22:58:26 -0300

    Add files via upload
    Dante subió el programa HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        HolaMundo();
+
+    }
+
+    public static void HolaMundo(){
+        System.out.println("Hola Mundo!!!!");
+    }
+
+}
