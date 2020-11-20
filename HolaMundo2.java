2b1778e31b2a9f10dbc9e98a2d29361a9e9eb261
Danthexz <74031922+Danthexz@users.noreply.github.com>
2020-11-19 23:07:49 -0300

    Add files via upload
    Dante subió HolaMundo v2

---
+++ HolaMundo2.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        HolaMundo();
+
+    }
+
+    public static void HolaMundo(){
+        System.out.println("Hola Mundo!!!!");
+        System.out.println("Dante Alvarez");
+    }
+
+}
